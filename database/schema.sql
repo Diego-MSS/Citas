85c8be4e170e75313116cf20e8b4f37109972bda
Diego-MSS <diego.mora@smartick.com>
2025-10-28 12:32:01 +0100

    Esquema de la base de datos y los datos estaticos

---
+++ database/schema.sql
@@ -0,0 +1,24 @@
+set names utf8mba;
+set FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS CITA;
+DROP TABLE IF EXISTS USUARIO;
+DROP TABLE IF EXISTS SLOTS;
+
+Create table USUARIO( 
+id int auto_increment primary key, 
+login varchar(250), 
+pass varchar(250), 
+nombre varchar(250));
+create table SLOTS( 
+id int auto_increment primary key, 
+hora time);
+create table CITA( 
+id int auto_increment primary key, 
+fecha date, 
+hora int,
+asunto Varchar(250) 
+usuario int, 
+estado enum(’RESERVADA’, ‘CONFIRMADA’,CANCELADA’,’FINALIZADA’) DEFAULT ‘RESERVADA’,
+foreign key (hora) references SLOTS (id), 
+foreign key (usuario) references USUARIO (id));
